37e1690eb09eee8339e30773a55efdd399a4b335
Syeda Ismat Farjana <syeda.ismat.farjana@gmail.com>
2023-02-20 13:44:53 +1100

    add middleware

--- myApp.js
+++ myApp.js
@@ -1,10 +1,22 @@
 let express = require("express");
 require("dotenv").config();
 let app = express();
+const IP = require("ip");
+const ipAddress = IP.address();
 
 console.log("Hello World");
 
-app.get("/", (req, res) => {
+// middleware
+const middleware = function (req, res, next) {
+  const method = req.method;
+  const path = req.path;
+  const ip = ipAddress;
+  const string = `${method} ${path} - ${ip}`;
+  console.log(string);
+  next();
+};
+
+app.get("/", middleware, (req, res) => {
   const absolutePath = __dirname + "/views/index.html";
   // res.send("Hello Express");
   res.sendFile(absolutePath);
@@ -12,7 +24,7 @@ app.get("/", (req, res) => {
 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/json", (req, res) => {
+app.get("/json", middleware, (req, res) => {
   if (process.env.MESSAGE_STYLE == "uppercase") {
     res.json({ message: "HELLO JSON" });
   } else {
